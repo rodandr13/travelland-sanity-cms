751605fc2f780013870bfebdaa2b372c5601c605
Andrey Roditelev <rodandr13@gmail.com>
2024-03-04 22:41:17 +0300

    feat: add schema for excursion

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {media} from 'sanity-plugin-media'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: 'w6lmtfgf',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), media()],
 
   schema: {
     types: schemaTypes,
--- schemas/excursion.ts
+++ schemas/excursion.ts
@@ -4,16 +4,89 @@ export default defineType({
   name: 'excursion',
   type: 'document',
   title: 'Excursion',
+  groups: [
+    {
+      name: 'content',
+      title: 'Content',
+      default: true,
+    },
+    {
+      name: 'seo',
+      title: 'SEO',
+    },
+    {
+      name: 'media',
+      title: 'Media',
+    },
+  ],
   fields: [
     defineField({
       name: 'name',
       type: 'string',
       title: 'Name',
+      group: 'content',
     }),
     defineField({
       name: 'slug',
       type: 'string',
       title: 'Slug',
+      group: 'content',
+    }),
+    defineField({
+      name: 'description',
+      type: 'array',
+      title: 'Description',
+      group: 'content',
+      of: [
+        {
+          type: 'block',
+          lists: [
+            {title: 'Bullet', value: 'bullet'},
+            {title: 'Numbered', value: 'number'},
+          ],
+        },
+      ],
+    }),
+    defineField({
+      name: 'gallery',
+      type: 'array',
+      title: 'Gallery',
+      group: 'media',
+      of: [
+        {
+          type: 'image',
+          fields: [
+            {
+              name: 'caption',
+              type: 'string',
+              title: 'Caption',
+            },
+            {
+              name: 'attribution',
+              type: 'string',
+              title: 'Attribution',
+            },
+          ],
+          options: {
+            hotspot: true,
+          },
+        },
+      ],
+      options: {
+        layout: 'grid',
+      },
+    }),
+    defineField({
+      name: 'seoTitle',
+      type: 'string',
+      title: 'Title',
+      group: 'seo',
+    }),
+    defineField({
+      name: 'seoDescription',
+      type: 'text',
+      title: 'Description',
+      group: 'seo',
     }),
   ],
 })
